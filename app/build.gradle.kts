b770b160f4da53f7f55820933cf4d5fe245136fc
onuralan <onuralann20@gmail.com>
2024-12-17 21:23:10 +0300

    retrofit dep eklendi.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,6 @@ dependencies {
     implementation("org.json:json:20230227")
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 }
